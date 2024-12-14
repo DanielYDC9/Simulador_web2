0f8962a021727f75d5cda479964ae76ce5f26d4f
DanielYDC9 <97945493+DanielYDC9@users.noreply.github.com>
2024-12-14 12:14:34 +0100

    Update CitaMedica.cpp

--- Simulador_web2/src/CitaMedica.cpp
+++ Simulador_web2/src/CitaMedica.cpp
@@ -1,17 +1,22 @@
 #include "../include/CitaMedica.hpp"
 
 
-CitaMedica::CitaMedica(int idPaciente, const std::string& fecha) {
-    : idPaciente(idPaciente), fecha(fecha) {}
+// Constructor de la clase CitaMedica
+CitaMedica::CitaMedica(int idPaciente, const std::string& fecha)
+    : idPaciente(idPaciente), fecha(fecha) {
+}
 
+// Método que retorna el ID del paciente
 int CitaMedica::getIdPaciente() const {
     return idPaciente;
 }
 
+// Método que retorna la fecha de la cita
 const std::string& CitaMedica::getFecha() const {
     return fecha;
 }
 
+// Método que muestra la información de la cita médica
 void CitaMedica::mostrarInformacion() const {
     std::cout << "Cita - ID Paciente: " << idPaciente << ", Fecha: " << fecha << std::endl;
 }
