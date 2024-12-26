86dcb7ac2ae15f735f493db0860615e7479a1742
DanielYDC9 <97945493+DanielYDC9@users.noreply.github.com>
2024-12-26 21:01:00 +0100

    Update CitaMedica.hpp

--- Simulador_web2/include/CitaMedica.hpp
+++ Simulador_web2/include/CitaMedica.hpp
@@ -1,21 +1,23 @@
 #ifndef CITAMEDICA_HPP
 #define CITAMEDICA_HPP
-
 #include <iostream>
 #include <string>
 
 class CitaMedica {
 private:
-    int idPaciente;         // ID del paciente asociado a la cita
+    int idPaciente;  
+    int idMedico;
     std::string fecha;      // Fecha de la cita
-
+    std::string motivo;
 public:
     // Constructor
-    CitaMedica(int idPaciente, const std::string& fecha);
+    CitaMedica(int idPaciente, int idMedico, const std::string& fecha, const std::string& motivo);
 
     // Métodos de acceso
-    int getIdPaciente() const;                  // Retorna el ID del paciente
-    const std::string& getFecha() const;        // Retorna la fecha de la cita
+    int getIdPaciente() const;  // Retorna el ID del paciente
+    int getIdMedico() const // Retorna el ID del Medico
+    std::string& getFecha() const;        // Retorna la fecha de la cita
+    std::string getMotivo() const;  // Devuelve el motivo de cita medica
 
     // Método para mostrar la información de la cita
     void mostrarInformacion() const;
