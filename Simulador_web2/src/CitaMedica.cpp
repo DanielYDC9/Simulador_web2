b1aa154a0ba6808604ce37b4eedc58eddcaab984
DanielYDC9 <97945493+DanielYDC9@users.noreply.github.com>
2025-01-16 18:43:56 +0100

    Update CitaMedica.cpp

--- Simulador_web2/src/CitaMedica.cpp
+++ Simulador_web2/src/CitaMedica.cpp
@@ -20,4 +20,12 @@ void CitaMedica::setFecha(const std::string& nuevaFecha) {
 void CitaMedica::setMotivo(const std::string& nuevoMotivo) {
     motivo = nuevoMotivo;
 }
-
+void CitaMedica::mostrarInformacion() const {
+    std::cout << "===============================\n";
+    std::cout << "ID Cita: " << idCita << "\n";
+    std::cout << "ID Paciente: " << idPaciente << "\n";
+    std::cout << "ID Medico: " << idMedico << "\n";
+    std::cout << "Fecha: " << fecha << "\n";
+    std::cout << "Motivo: " << motivo << "\n";
+    std::cout << "===============================\n";
+}
