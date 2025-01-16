4191d3379189227271453e0a679a54e45ccdec5e
DanielYDC9 <97945493+DanielYDC9@users.noreply.github.com>
2025-01-16 18:40:35 +0100

    Update SistemaHospital.hpp

--- Simulador_web2/include/SistemaHospital.hpp
+++ Simulador_web2/include/SistemaHospital.hpp
@@ -4,37 +4,35 @@
 #include "../include/Paciente.hpp"
 #include "../include/Medico.hpp"
 #include "../include/CitaMedica.hpp"
+#include <iostream>
 #include <vector>
 #include <string>
 
 class SistemaHospital {
+public:
+    SistemaHospital();
+    ~SistemaHospital();
+//Gestion Pacientes
+    void agregarPaciente(const std::string& id, const std::string& nombre, int edad);
+    void eliminarPaciente(const std::string& id);
+    void buscarPaciente(const std::string& id) const;
+    void mostrarPacientes() const;
+//Gestion Medicos
+    void agregarMedico(const std::string& id, const std::string& nombre, const std::string& especialidad, int edad);
+    void eliminarMedico(const std::string& id);
+    void buscarMedico(const std::string& id) const;
+    void mostrarMedicos() const;
+    // Gestión de citas
+    void agregarCita(const std::string& idCita, const std::string& idPaciente, const std::string& idMedico, const std::string& fecha, const std::string& motivo);
+    void cancelarCita(const std::string& idCita);
+    void mostrarCitas() const;
+// Manejo de archivos
+    void guardarDatos() const;
+    void cargarDatos();
 private:
     std::vector<Paciente> pacientes;  
     std::vector<Medico> medicos;      
     std::vector<CitaMedica> citas;
 
-    void cargarPacientes();
-    void cargarMedicos();
-    void cargarCitas();
-    void guardarPacientes() const;
-    void guardarMedicos() const;
-    void guardarCitas() const;
-
-public:
-    SistemaHospital();
-    ~SistemaHospital();
-
-    void agregarPaciente();
-    void agregarMedico();
-    void asignarCita();
-    void mostrarPacientes() const;
-    void mostrarMedicos() const;
-    void mostrarCitas() const;
-
-    void guardarDatos();
-    bool existePaciente(int id) const;
-    bool existeMedico(int id) const;
-
-};
 
 #endif
